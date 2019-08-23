3dd84b71dc8b16034adcebc80bb43cd1951c9891
Theo Kandiliotis <Theo.Kandiliotis@thestreet.com>
2019-08-23 11:59:41 +0100

    Added test cases for story PlayerCanWinGame

---
+++ SnakesAndLadders.Test/MovingYourToken/PlayerCanWinTheGame.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace SnakesAndLadders.Test.MovingYourToken
+{
+    public class PlayerCanWinTheGame
+    {
+        [Test]
+        public void GivenTheTokenIsOnSquare97_WhenTheTokenIsMoved3Spaces_PlayerWinsGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void GivenTheTokenIsOnSquare97_WhenTheTokenIsMoved4Spaces_PlayerDoestNotWinGame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
